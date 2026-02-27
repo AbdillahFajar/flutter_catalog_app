6cef9f8ad161b7c0427fe053e7c606b37f82669e
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-02-27 15:51:54 +0700

    membuat halaman untuk menampilkan semua pesanan pelanggan dan tombol untuk hapus semua pesanan pelanggan

---
+++ lib/features/cart/presentation/pages/cart_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../cart/data/models/product_model.dart';
+
+class MyCart extends StatelessWidget {
+  const MyCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //context.watch, untuk membuat widget ini re-build tiap kali CartModel berubah
+    var cart = context.watch<CartModel>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Keranjang Kamu'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) =>
+                ListTile(
+                  leading: const Icon(Icons.fastfood),
+                  title: Text(cart.items[index])
+              ),
+            )
+          ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ElevatedButton(
+              onPressed: () => cart.removeAll(),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              child: const Text(
+                'Batalkan Semua Pesanan',
+                style: TextStyle(
+                  color: Colors.white
+                ),
+              ),
+            ),
+          ),
+        ]
+      )
+    );
+  }
+}
