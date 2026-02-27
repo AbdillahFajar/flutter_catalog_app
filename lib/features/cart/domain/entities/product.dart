148702861f174d5a3632e7947d6364591f9cee21
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-02-28 06:13:02 +0700

    membuat class Product

---
+++ lib/features/cart/domain/entities/product.dart
@@ -0,0 +1,13 @@
+//Membuat class Product 
+
+class Product {
+  final String id;
+  final String name;
+  final String price;
+
+  const Product({
+    required this.id,
+    required this.name,
+    required this.price
+  });
+}
